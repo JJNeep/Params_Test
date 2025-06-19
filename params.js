d99ef5a1068dde661b667d8cfadaaccdee015c2f
JJNeep <grosvenor.streaming@gmail.com>
2025-06-19 14:08:43 +0800

    Create params.js

---
+++ params.js
@@ -0,0 +1,13 @@
+// Function to get URL parameters as an object
+function getUrlParams() {
+  const params = new URLSearchParams(window.location.search);
+  const paramsObj = {};
+  for (const [key, value] of params.entries()) {
+    paramsObj[key] = value;
+  }
+  return paramsObj;
+}
+
+// Usage
+const urlParams = getUrlParams();
+console.log(urlParams);
